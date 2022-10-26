a5e1d4f3af8e174322c6b238fc8a86433e614bf6
Juan Romero <juan.romero5822@gmail.com>
2022-10-26 16:15:48 -0500

    Final Commit

--- mu/connection/connection.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-# Cargo el certificado de mi proyecto firebase
-firebase_sdk = credentials.Certificate('data.json')
-#Hacemos referencia a la base de datos en tiempo real de firebasse
-firebase_admin.initialize_app(firebase_sdk,{'databaseURL':'https://app-python-44c9b-default-rtdb.firebaseio.com/'})
